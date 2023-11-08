38b63f59b3b447178608c8a971b71e5f9bc8769b
dddiias <115692320+dddiias@users.noreply.github.com>
2023-11-09 02:13:33 +0600

    upd: updated cart with observer function

--- cart/cart.go
+++ cart/cart.go
@@ -1,32 +1,64 @@
 package cart
 
 import (
-    "sync"
-    "shop/product"
+	"shop/product"
+	"sync"
 )
 
 type Cart struct {
-    contents   []product.Product
-    totalPrice float64
+	contents   []product.Product
+	totalPrice float64
+	observers  []Observer
 }
 
 var (
-    cartInstance *Cart
-    once sync.Once
+	cartInstance *Cart
+	once         sync.Once
 )
 
 func GetCartInstance() *Cart {
-    once.Do(func() {
-        cartInstance = &Cart{}
-    })
-    return cartInstance
+	once.Do(func() {
+		cartInstance = &Cart{}
+	})
+	return cartInstance
 }
 
 func (c *Cart) AddToCart(p product.Product) {
-    cartInstance.contents = append(cartInstance.contents, p)
-    cartInstance.totalPrice += p.Price
+	cartInstance.contents = append(cartInstance.contents, p)
+	cartInstance.totalPrice += p.Price
+	c.NotifyObservers()
+}
+
+func (c *Cart) RemoveFromCart(p product.Product) {
+	for i, item := range c.contents {
+		if item == p {
+			c.contents = append(c.contents[:i], c.contents[i+1:]...)
+			c.totalPrice -= p.Price
+			c.NotifyObservers()
+			break
+		}
+	}
 }
 
 func (c *Cart) GetTotalPrice() float64 {
-    return cartInstance.totalPrice
+	return cartInstance.totalPrice
+}
+
+func (c *Cart) Attach(observer Observer) {
+	c.observers = append(c.observers, observer)
+}
+
+func (c *Cart) Detach(observer Observer) {
+	for i, o := range c.observers {
+		if o == observer {
+			c.observers = append(c.observers[:i], c.observers[i+1:]...)
+			break
+		}
+	}
+}
+
+func (c *Cart) NotifyObservers() {
+	for _, observer := range c.observers {
+		observer.Update(c)
+	}
 }
