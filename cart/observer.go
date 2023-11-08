e410abd0f453c32f233d57bab4424da78448aa3a
dddiias <115692320+dddiias@users.noreply.github.com>
2023-11-09 02:11:37 +0600

    add: observer pattern

---
+++ cart/observer.go
@@ -0,0 +1,19 @@
+package cart
+
+import (
+	"fmt"
+)
+
+type Observer interface {
+	Update(*Cart)
+}
+
+type CartObserver struct {
+	Name string
+}
+
+func (co CartObserver) Update(cart *Cart) {
+	fmt.Println("------------------------------------------------------")
+	fmt.Printf("Покупатель %s: Корзина была обновлена. Общая цена: $%.2f\n", co.Name, cart.totalPrice)
+	fmt.Println("------------------------------------------------------")
+}
